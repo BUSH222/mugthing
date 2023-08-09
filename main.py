c7c68ef330c0128ba1f6ea1d3cebaa13233365c9
Ted V <58144503+BUSH222@users.noreply.github.com>
2023-08-10 01:18:48 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageDraw, ImageFont
+import requests
+from io import BytesIO
+from rembg import remove
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://thispersondoesnotexist.com/")
+face = Image.open(BytesIO(response.content))
+
+
+width, height = face.size
+face = face.crop((0, 0, width, height))
+face_data = remove(face)
+face = face_data
+
+new_width = new_height = 700
+border_size = 162
+
+newface = Image.new('RGBA', (new_width + border_size * 2, new_height + border_size * 2), (0, 0, 0, 0))
+newface.paste(face.resize((new_width, new_height)), (border_size, border_size*2))
+
+background = Image.open("mugshot_background.png")
+background = background.resize((width, height))
+background.paste(newface, (0, 0), newface)
+
+
+response2 = requests.get("https://dog.ceo/api/breeds/image/random")
+
+img2 = Image.open(BytesIO(requests.get(response2.json()["message"]).content))
+img2 = img2.resize((1024, 1024))
+
+new_image = Image.new('RGBA', (2048, 1024))
+
+new_image.paste(background, (0, 0))
+new_image.paste(img2, (1024, 0))
+
+new_image.show()
+new_image.save('final.png')
